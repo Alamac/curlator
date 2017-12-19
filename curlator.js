38a9eda1cd3693e71c1af28c8fa2074d1c8bf6ac
Alamac <alamacus@gmail.com>
2017-12-19 18:56:20 +0300

    Initial Commit

---
+++ curlator.js
@@ -0,0 +1,29 @@
+var a = process.argv[2];
+makeCurl(getHeaders(a));
+
+function getHeaders(d) {
+    var c = JSON.parse(d);
+    var headers = c.data[0]['headers.header'],
+        values = c.data[0]['headers.value'],
+        headerObject = {};
+    headerObject['request_uri'] = c.data[0]['request_uri'];
+    for (var i = 0; i < headers.length; i++) {
+        headerObject[headers[i]] = values[i];
+    }
+    return headerObject;
+}
+
+function makeCurl(hObj) {
+    var hString = '',
+        curlString = '';
+    for (var key in hObj) {
+        if (key != 'request_uri') {
+            hString += "-H '" + key + ": " + hObj[key] + "' ";
+        }
+    }
+    curlString = "curl -i 'https://ads.adfox.ru" +
+        hObj.request_uri +
+        "' " +
+        hString;
+    return console.log(curlString);
+}
